9693bff5d8c18809a1f9f736bc9923060e5cc3e5
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2024-10-08 21:08:45 +0300

    hw3

---
+++ js.js
@@ -0,0 +1,18 @@
+let result = 5 + 5 + '5';
+console.log(result);
+
+let gmail = 'max.ilchyshyn@gmail.com'
+console.log(gmail);
+console.log(gmail.length);
+let gmailCheck = gmail.includes('@');
+console.log(gmailCheck);
+
+let i = 'My';
+let word = 'name';
+let secondWord = 'is'
+let myName = 'Maksym'
+let fullName = i + word + secondWord + myName;
+
+let userName = prompt("Яке ваше і'мя????")
+let userPayment = prompt("Скільки ви б хотіли задонатити?????")
+console.log("Дякуємо," + userName + "! До сплати " + userPayment + " гривень")
